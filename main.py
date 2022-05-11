c87d4fb39ccf38659335d2d1d94799252ed75f96
Atsushi0803 <atsushi.7624@gmail.com>
2022-05-11 11:15:24 +0900

    マーカーを指定する要素を追加

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ import functions
 def main():
     functions.set_rc_params()
 
-    plot1 = PlotData("plot1")
-    plot1.set_data("data1", [0, 1, 2], [4, 5, 6])
-    plot1.set_labels("XLabel", "YLabel")
-    plot1.set_limits([0, 2], [0, 6])
+    plot1 = PlotData("css-apcma_sf7_lp")
+    plot1.set_data("BW:250kHz", list(range(1, 7)), [0, 0, 0, 0.005025538, 0.00568108, 0.009944424])
+    plot1.set_data("BW:1000kHz",  list(range(1, 7)), [0, 0, 0, 9.40E-05, 0.00123092, 0.00020896])
+    plot1.set_labels("Number of TX", "Loss Probability")
+    # plot1.set_limits([0, 2], [0, 6])
 
     fig1 = plt.figure(figsize=(6, 4))
     ax1 = fig1.add_subplot(111)
     for i in range(plot1.N_data):
-        ax1.scatter(plot1.x[i], plot1.y[i], label=plot1.data_label[i])
+        ax1.scatter(plot1.x[i], plot1.y[i], label=plot1.data_label[i], marker=plot1.markers[i])
         # ax1.plot(plot1.x[i], plot1.y[i], label=plot1.data_label[i])
     ax1.set_xlabel(plot1.x_label)
     ax1.set_ylabel(plot1.y_label)
@@ -42,6 +43,8 @@ class PlotData:
         self.data_label = []
         self.x = []
         self.y = []
+
+        self.markers = ["o", "x"]
 
     def set_data(self, label, x, y):
         self.N_data += 1
